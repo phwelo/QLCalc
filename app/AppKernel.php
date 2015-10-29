37fde4805319bb9089cfa24029a8a40bcd8ce766
phwelo <phwelo@gmail.com>
2015-10-29 12:38:19 -0400

    Pieced back together after reorg of functions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new QL\CalcBundle\QLCalcBundle(),
-            new AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/QL/CalcBundle/Controller/DefaultController.php
+++ src/QL/CalcBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 class Calculation extends Controller
 {
   // simple function to verify existence of an item in post
-  public function checkExist($postItem){
+  private function checkExist($postItem){
     if(isset($_POST[$postItem])){
       return true;
     } else {
@@ -20,10 +20,10 @@ class Calculation extends Controller
   // end checkExist
 
   // replace operator characters with proper PHP math operators
-  public function replaceOperators($string){
+  private function replaceOperators($string){
     if($this->checkExist('expression')){
-      $string = str_replace("x","*",$Expression);
-      $string = str_replace("÷","/",$Expression);
+      $string = str_replace("x","*",$string);
+      $string = str_replace("÷","/",$string);
       return $string;
     }
   }
@@ -31,43 +31,39 @@ class Calculation extends Controller
 
   // test to make sure we have no letters in post data
   // return true for "ok" and false for "insecure"
-  public function checkForTampering($Expression){
-    if($this->checkExist('expression')){
-      $explodedPost = str_split($Expression);
-      foreach ($explodedPost as &$letter){
-        if(preg_match("/[A-Za-z]/", $letter, $MatchAlpha)){
-          return false;
-        } else {
-          return true;
-        }
+  private function checkForTampering($Expression){
+    $explodedPost = str_split($this->replaceOperators($Expression));
+    foreach ($explodedPost as &$letter){
+      if(preg_match("/[A-Za-z]/", $letter, $MatchAlpha)){
+        return false;
+      } else {
+        return true;
       }
-    } else {
-      return false;
     }
   }
+  // end checkForTampering
 
   // Evaluate the contents of the expression now that it has been filtered to
   // prevent invalid operators and user input
   public function evaluateExpression($Expression){
+    if($this->checkForTampering($Expression))
     $Expression = $this->replaceOperators($Expression);
     $Evald = eval("return ($Expression);");
     return $Evald;
   }
-  // end parse & eval
+  // end evaluateExpression
 }
+// end Calculation class
 
 class DefaultController extends Controller
 {
   public function indexAction()
   {
-    // Instantiate Calculation and only run parseExpression after we've
-    // tested the post value to assure it doesn't have any code in it
-    // since eval is potentially dangerous
     $Calc1 = new Calculation();
-
+    $screen = $Calc1->evaluateExpression($_POST['expression']);
       // If we get anything unexpected (or nothing, like new load of page) then
       // we want to show nothing on the calculator screen
-      $screen = "";
+
 
     return $this->render('QLCalcBundle:Default:index.html.twig', array(
     'screen' => $screen,));
